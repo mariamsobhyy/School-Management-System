ccb02c50e9b2108d409a7b2fb216035de6e8b194
Mariam.Sobhy <Mariam.Sobhy@SIS-SR-ADAMM>
2026-09-21 15:18:35 +0300

    ..

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using School_Management_System.Data;
 using School_Management_System.DTOs.DepartmentDto;
+using School_Management_System.Mapper;
 using School_Management_System.Models;
 
 namespace School_Management_System.Controllers
@@ -11,85 +15,97 @@ namespace School_Management_System.Controllers
     {
         private readonly AppDbContext _context;
 
+        private readonly IMapper _mapper;
+
+
+
         public DepartmentController(AppDbContext context)
         {
             _context = context;
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<DepartmentProfile>());
+            _mapper = config.CreateMapper();
         }
 
-       
+
+
+
         [HttpGet]
         public IActionResult GetDepartments()
         {
             List<Department> departments = _context.Departments.ToList();
 
-            List<DepartmentDto> result = new List<DepartmentDto>();
+            /* List<DepartmentDto> result = new List<DepartmentDto>();
 
-            foreach (Department department in departments)
+             foreach (Department department in departments)
+             {
+                 result.Add(new DepartmentDto
+                 {
+                     Id = department.Id,
+                     Name = department.Name,
+                     Description = department.Description
+                 });
+             }*/
+
+
+
+            List<DepartmentDto> departmentDTOs = _mapper.Map<List<DepartmentDto>>(departments);
+            if (departments == null || departments.Count == 0)
             {
-                result.Add(new DepartmentDto
-                {
-                    Id = department.Id,
-                    Name = department.Name,
-                    Description = department.Description
-                });
+                return NotFound("No departments found");
             }
-
-            return Ok(result);
+            return Ok(departmentDTOs);
         }
 
-        
+
+
+
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            Department department = _context.Departments.Find(id);
+            var dept = _context.Departments .FirstOrDefault(a => a.Id == id);
 
-            if (department == null)
-            {
-                return NotFound();
-            }
-
-            DepartmentDto result = new DepartmentDto
-            {
-                Id = department.Id,
-                Name = department.Name,
-                Description = department.Description
-            };
-
-            return Ok(result);
+            var dep = _mapper.Map<DepartmentDto>(dept);
+            return Ok(dep);
         }
 
-       
         [HttpPost]
-        public IActionResult CreateDepartment(CreateDepartmentDto dto)
+        public IActionResult CreateDepartment(CreateDepartmentDto createDepartmentDTO)
         {
-            Department department = new Department
+            if (createDepartmentDTO == null)
             {
-                Name = dto.Name,
-                Description = dto.Description
-            };
+                return BadRequest("Department cannot be null");
 
-            _context.Departments.Add(department);
+
+            }
+            //var department = new Department
+            //{
+            //    Name = createDepartmentDTO.Name,
+            //    Description = createDepartmentDTO.Description
+            //};
+
+            var dep = _mapper.Map<Department>(createDepartmentDTO);
+            _context.Departments.Add(dep);
             _context.SaveChanges();
-
-            DepartmentDto result = new DepartmentDto
-            {
-                Id = department.Id,
-                Name = department.Name,
-                Description = department.Description
-            };
-
             return CreatedAtAction(
                 nameof(GetById),
-                new { id = department.Id },
-                result
-            );
+                new { id = dep.Id },
+                dep
+                );
         }
 
-       
+        [HttpGet("Withdept")]
+
+        public IActionResult GetDeptWithTeacher()
+        {
+            var m = _context.Departments.Include(t => t.teachers).ToList();
+            return Ok(m);
+        }
+
+
+
         [HttpPut("{id}")]
-        public IActionResult UpdateDepartment(
-            int id,
-            UpdateDepartmentDto dto)
+        public IActionResult UpdateDepartment(int id,UpdateDepartmentDto dto)
         {
             Department department = _context.Departments.Find(id);
 
---
+++ Mapper/DepartmentProfile.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.Identity.Client;
+using System.Runtime;
+using School_Management_System.Models;
+using School_Management_System.DTOs.DepartmentDto;
+
+namespace School_Management_System.Mapper
+{
+    public class DepartmentProfile : Profile
+
+
+    {
+        public DepartmentProfile()
+        {
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+            CreateMap <Department, CreateDepartmentDto>().ReverseMap();
+
+        }
+    }
+
+   
+
+
+    
+}
